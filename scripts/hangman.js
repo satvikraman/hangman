d64b00595d645103ae1065023e68ca4eb5b30b30
Arvind Raman <ramanarvind@gmail.com>
2021-08-02 09:59:35 +0530

    german words, parse csv file, added conditional statements

--- scripts/hangman.js
+++ scripts/hangman.js
@@ -5,12 +5,29 @@ class hangman {
         this.rightAns = 0;
         this.parseCsvFile();
     }
+
+    choosePuzzle() {
+        let randomRow = Math.floor(Math.random()*this.puzzles.data.length);
+        console.log(this.puzzles.data[randomRow][0],  this.puzzles.data[randomRow][1]);
+    }
+
+    // parse csv file and store all the puzzles
     parseCsvFile() {
         // Parse local CSV file
-        let csvString = "Apple,Fruit\nFootball, Team sport"
-        let result = Papa.parse(csvString);
-        console.log(result.data.length);
+        if (this.lang == "english") {
+            let csvFile = "https://satvikraman.github.io/hangman/assets/english.csv";
+        } else {
+            let csvFile = "https://satvikraman.github.io/hangman/assets/german.csv";
+        }
+        Papa.parse(csvFile, 
+                    {
+                        download: true,
+                        complete: result => {
+                            this.puzzles = result;
+                            this.choosePuzzle();
+                        }
+                    });
     }
 }
 
-game = new hangman ("english");
+game = new hangman("english");
